c154c83fe6c1a3f04dca08d7199733ca4d45cd91
Reuven Svechin <rsvechin@liveperson.com>
2022-12-12 19:56:34 -0600

    Initial commit

---
+++ src/main/java/reuven/svechin/winnipegmosquitos/WinnipegMosquitoesApplication.java
@@ -0,0 +1,14 @@
+package reuven.svechin.winnipegmosquitos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WinnipegMosquitoesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WinnipegMosquitoesApplication.class, args);
+        System.out.println("UP");
+    }
+
+}
---
+++ src/test/java/reuven/svechin/winnipegmosquitos/WinnipegMosquitosApplicationTests.java
@@ -0,0 +1,13 @@
+package reuven.svechin.winnipegmosquitos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WinnipegMosquitoesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
