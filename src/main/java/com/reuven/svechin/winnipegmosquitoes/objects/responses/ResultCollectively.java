ff18f7fc3f01ddab6714f51c619ac7b7ef879f82
Reuven Svechin <rsvechin@liveperson.com>
2022-12-17 15:10:41 -0600

    remove unnecessary classes, added java doc, code analyses.

--- src/main/java/com/reuven/svechin/winnipegmosquitoes/objects/responses/ResultCollectively.java
+++ src/main/java/com/reuven/svechin/winnipegmosquitoes/objects/responses/ResultCollectively.java
@@ -3,6 +3,9 @@ package com.reuven.svechin.winnipegmosquitoes.objects.responses;
 import lombok.Getter;
 import lombok.Setter;
 
+/**
+ * The class represents data to display if "COLLECTIVELY" option chosen.
+ */
 @Setter
 @Getter
 public class ResultCollectively extends Result {
